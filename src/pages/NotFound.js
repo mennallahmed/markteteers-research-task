fcd69b9aad35593d03dbe13750f402388f868a04
MennaAllah Ahmed <78885127+mennallahmed@users.noreply.github.com>
2022-11-29 19:07:18 +0200

    add Not Found page

---
+++ src/pages/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div>
+      <img className='mt-5' alt='not found' src='https://lh6.googleusercontent.com/Bu-pRqU_tWZV7O3rJ5nV1P6NjqFnnAs8kVLC5VGz_Kf7ws0nDUXoGTc7pP87tyUCfu8VyXi0YviIm7CxAISDr2lJSwWwXQxxz98qxVfMcKTJfLPqbcfhn-QEeOowjrlwX1LYDFJN'/>
+    </div>
+  )
+}
+
+export default NotFound
