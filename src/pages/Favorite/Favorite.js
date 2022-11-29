4b9174000ba0b4dd68b0981284671557597ee1b3
MennaAllah Ahmed <78885127+mennallahmed@users.noreply.github.com>
2022-11-29 21:43:31 +0200

    add counter reducer

--- src/pages/Favorite/Favorite.js
+++ src/pages/Favorite/Favorite.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment } from '../../Redux/counter/counterSlice'
 
 const Favorite = () => {
+  const count = useSelector((state) => state.counter.value)
+  const dispatch = useDispatch()
+
   return (
-    <div>Favorite</div>
+    <div>
+      <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
+    </div>
   )
 }
 
