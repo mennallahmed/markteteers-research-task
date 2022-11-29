0203a53b5359e93be83d2dc9ee1edd929a326d1a
MennaAllah Ahmed <78885127+mennallahmed@users.noreply.github.com>
2022-11-29 19:06:56 +0200

    add pages routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from "./pages/Home/Home";
+import Favorite from './pages/Favorite/Favorite';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<> <Home/> </>}/>
+        <Route path='/home' element={<> <Home/> </>}/>
+        <Route path='/favorites' element={<> <Favorite/> </> }/>
+        <Route path='*' element={<NotFound/>}/>
+        <Route path='/book-details/:id' element={<Home/> }/>
+      </Routes>
+      <Footer/>
      
     </div>
   );
